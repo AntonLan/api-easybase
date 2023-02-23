9e2a933e30ed13da18fabafe90f1efeb9345eaf9
AntonLan <lanqel@gmail.com>
2023-02-23 16:36:41 +0700

    initial

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+	const PORT = process.env.PORT || 5002
+	const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+	await app.listen(PORT, () => console.log(`Server start on ${PORT}`))
+}
+
+bootstrap()
